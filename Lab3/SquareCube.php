32e41d2727885774f55a723f6c5aafc2b8cd8758
Minh Tan <minhtan@Minhs-MacBook-Pro.local>
2021-10-13 16:07:27 +0700

    exercise lab3

---
+++ Lab3/SquareCube.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Square Cup</title>
+</head>
+<body>
+    <font size="5" color="blue">
+        Generate Square and Cube Values
+    </font>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>">
+        <table> 
+            <tr>
+                <td>Select Start Number: </td>
+                <td>
+                    <select name="start">
+                        <?php
+                            for ($i = 0; $i < 10; $i++) {
+                                print("<option>$i</option>");
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Select End Number: </td>
+                <td>
+                    <select name="end">
+                        <?php
+                            for ($i = 0; $i < 20; $i++) {
+                                print("<option>$i</option>");
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td align="right">
+                    <input type="submit" value="Submit">
+                </td>
+                <td align="left">
+                    <input type="reset" value="Reset">
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <th>Number</th>
+                <th>Square</th>
+                <th>Cube</th>
+            </tr>
+            <?php
+                if (array_key_exists("start", $_GET)) {
+                    $start = $_GET("start");
+                    $end = $_GET("end");
+                    $i = $start;
+                    while($i <= $end) {
+                        $sqr = $i * $i;
+                        $cubed = $i * $i * $i;
+                        print "<tr><td>$i</td><td>$sqr</td><td>$cubed</td></tr>";
+                        $i = $i + 1;
+                    }
+                }
+            ?>
+        </table>
+    </form>
+</body>
+</html>
